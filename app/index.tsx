7a5c015df53afc4dccd72298a5987814dbfcff7b
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-12-20 10:00:59 +0600

    Add initial project structure with authentication and profile screens

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+
+const Layout = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack text-red-400">RootLayout </Text>
+      <StatusBar style="dark" />
+      <Link href={"/profile"} style={{ color: "blue" }}>
+        Go to profile
+      </Link>
+    </View>
+  );
+};
+
+export default Layout;
